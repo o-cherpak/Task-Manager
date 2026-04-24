a259885aea253491c3d0cff3add6cdde388ba095
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-04-24 12:30:39 +0200

    add TodoItem.cs, TodoStatus.cs

---
+++ TaskManager/Models/TodoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskManager.Models;
+
+public record TodoItem(
+    Guid Id,
+    string Title,
+    TodoStatus Status,
+    DateTime CreatedAt
+);
---
+++ TaskManager/Models/TodoStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskManager.Models;
+
+public enum TodoStatus
+{
+    Pending,
+    InProgress,
+    Done
+}
