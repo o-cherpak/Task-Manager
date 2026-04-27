2bcd717bf4297131df65b0bb2e6654776ffb6b46
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-04-27 13:46:57 +0200

    add deadline migration to db

---
+++ TaskManager/Migrations/20260427114345_AddDeadline.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeadline : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeadLine",
+                table: "TodoItems",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeadLine",
+                table: "TodoItems");
+        }
+    }
+}
