400777f0b4d21f40750161e182962f598a620a69
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-04-27 19:01:47 +0200

    create Category

---
+++ TaskManager/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskManager.Models;
+
+public class Category
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public ICollection<TodoItem> Todos { get; set; } = new List<TodoItem>();
+}
