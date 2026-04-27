613b4879f7270d2e6612ae1e427aeba2c4301c77
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-04-27 13:04:40 +0200

    add Priority tests, update db (In memory db)

--- TaskManager.Tests/TodoManagerTest.cs
+++ TaskManager.Tests/TodoManagerTest.cs
@@ -1,18 +1,29 @@
-﻿using TaskManager.Exceptions;
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Exceptions;
 using TaskManager.Models;
 using TaskManager.Services;
 
 namespace TaskManager.Tests;
 
-public class TodoManagerTest
+public class TodoManagerTest : IDisposable
 {
     private readonly TodoManager _tm;
     private readonly TodoDbContext _db;
 
     public TodoManagerTest()
     {
-        _db = new TodoDbContext();
+        var options = new DbContextOptionsBuilder<TodoDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+
+        _db = new TodoDbContext(options);
         _tm = new TodoManager(_db);
+    }
+
+    public void Dispose()
+    {
+        _db.Database.EnsureDeleted();
+        _db.Dispose();
     }
 
     [Theory]
@@ -24,8 +35,10 @@ public class TodoManagerTest
     public void AddTodoItemTest(string title)
     {
         var todo = _tm.AddTodoItem(title);
+        var todoWithHighPriority = _tm.AddTodoItem(title, TodoPriority.High);
 
         Assert.Equal(TodoStatus.Pending, todo.Status);
+        Assert.Equal(TodoPriority.High, todoWithHighPriority.Priority);
         Assert.Equal(title, todo.Title);
     }
 
@@ -107,5 +120,17 @@ public class TodoManagerTest
         _tm.SetComplete(todo1.Id);
 
         Assert.True(_tm.GetActive().Count() == 2);
+    }
+
+    [Fact]
+    void GetByPriorityTest()
+    {
+        var todoItem1 = _tm.AddTodoItem("todo1");
+        var todoItem2 = _tm.AddTodoItem("todo2", TodoPriority.High);
+        var todoItem3 = _tm.AddTodoItem("todo3", TodoPriority.High);
+
+        var result = _tm.GetPriority(TodoPriority.High).ToArray();
+
+        Assert.Equal([todoItem2, todoItem3], result);
     }
 }
--- TaskManager/TodoDbContext.cs
+++ TaskManager/TodoDbContext.cs
@@ -6,16 +6,27 @@ namespace TaskManager;
 
 public class TodoDbContext : DbContext
 {
+    public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
+    {
+    }
+
+    public TodoDbContext()
+    {
+    }
+
     public DbSet<TodoItem> TodoItems { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        var config = new ConfigurationBuilder()
-            .SetBasePath(AppContext.BaseDirectory)
-            .AddJsonFile("appsettings.json")
-            .Build();
+        if (!optionsBuilder.IsConfigured)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
 
-        optionsBuilder.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+            optionsBuilder.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+        }
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
