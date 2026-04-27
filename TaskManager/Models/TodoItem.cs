c612face2a01028d016069079ce3d0b11bb31f6e
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-04-27 19:04:59 +0200

    update todoItem with Category

--- TaskManager/Models/TodoItem.cs
+++ TaskManager/Models/TodoItem.cs
@@ -3,9 +3,11 @@
 public class TodoItem
 {
     public Guid Id { get; set; }
+    public Guid? CategoryId { get; set; }
     public string Title { get; set; } = string.Empty;
     public TodoStatus Status { get; set; }
     public TodoPriority Priority { get; set; }
+    public Category? Category { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? DeadLine { get; set; }
 }
